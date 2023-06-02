7786544a3e34c9c1e5473934ba96aedec33271ab
wasif_alam <wsfalam84@gmail.com>
2023-06-02 20:04:00 +0530

    added red color to toggle button

--- pages/candidates.vue
+++ pages/candidates.vue
@@ -44,20 +44,20 @@ const tableTdVisible = ref({
   isOwnerVisible: true,
 });
 
+const tdToggle = (state) => {
+  tableTdVisible.value.isRatingVisible = state;
+  tableTdVisible.value.isStagesVisible = state;
+  tableTdVisible.value.isTeamVisible = state;
+  tableTdVisible.value.isDateVisible = state;
+  tableTdVisible.value.isOwnerVisible = state;
+};
+
 const showAllTableTd = () => {
-  tableTdVisible.value.isRatingVisible = true;
-  tableTdVisible.value.isStagesVisible = true;
-  tableTdVisible.value.isTeamVisible = true;
-  tableTdVisible.value.isDateVisible = true;
-  tableTdVisible.value.isOwnerVisible = true;
+  tdToggle(true);
 };
 
 const hideAllTableTd = () => {
-  tableTdVisible.value.isRatingVisible = false;
-  tableTdVisible.value.isStagesVisible = false;
-  tableTdVisible.value.isTeamVisible = false;
-  tableTdVisible.value.isDateVisible = false;
-  tableTdVisible.value.isOwnerVisible = false;
+  tdToggle(false);
 };
 </script>
 
@@ -112,7 +112,7 @@ const hideAllTableTd = () => {
                       class="sr-only peer"
                     />
                     <div
-                      class="w-9 h-5 bg-gray-200 peer-focus:outline-none dark:peer-focus:ring-blue-800 rounded-full peer dark:bg-gray-600 peer-checked:after:translate-x-full peer-checked:after:border-white after:content-[''] after:absolute after:top-[2px] after:left-[2px] after:bg-white after:border-gray-300 after:border after:rounded-full after:h-4 after:w-4 after:transition-all dark:border-gray-500 peer-checked:bg-green-400"
+                      class="w-9 h-5 bg-red-400 peer-focus:outline-none dark:peer-focus:ring-blue-800 rounded-full peer dark:bg-gray-600 peer-checked:after:translate-x-full peer-checked:after:border-white after:content-[''] after:absolute after:top-[2px] after:left-[2px] after:bg-white after:border-gray-300 after:border after:rounded-full after:h-4 after:w-4 after:transition-all dark:border-gray-500 peer-checked:bg-green-400"
                     ></div>
                     <span
                       class="ml-3 text-sm font-normal text-gray-900 dark:text-gray-300"
@@ -135,7 +135,7 @@ const hideAllTableTd = () => {
                       class="sr-only peer"
                     />
                     <div
-                      class="w-9 h-5 bg-gray-200 peer-focus:outline-none dark:peer-focus:ring-blue-800 rounded-full peer dark:bg-gray-600 peer-checked:after:translate-x-full peer-checked:after:border-white after:content-[''] after:absolute after:top-[2px] after:left-[2px] after:bg-white after:border-gray-300 after:border after:rounded-full after:h-4 after:w-4 after:transition-all dark:border-gray-500 peer-checked:bg-green-400"
+                      class="w-9 h-5 bg-red-400 peer-focus:outline-none dark:peer-focus:ring-blue-800 rounded-full peer dark:bg-gray-600 peer-checked:after:translate-x-full peer-checked:after:border-white after:content-[''] after:absolute after:top-[2px] after:left-[2px] after:bg-white after:border-gray-300 after:border after:rounded-full after:h-4 after:w-4 after:transition-all dark:border-gray-500 peer-checked:bg-green-400"
                     ></div>
                     <span
                       class="ml-3 text-sm font-normal text-gray-900 dark:text-gray-300"
@@ -158,7 +158,7 @@ const hideAllTableTd = () => {
                       class="sr-only peer"
                     />
                     <div
-                      class="w-9 h-5 bg-gray-200 peer-focus:outline-none dark:peer-focus:ring-blue-800 rounded-full peer dark:bg-gray-600 peer-checked:after:translate-x-full peer-checked:after:border-white after:content-[''] after:absolute after:top-[2px] after:left-[2px] after:bg-white after:border-gray-300 after:border after:rounded-full after:h-4 after:w-4 after:transition-all dark:border-gray-500 peer-checked:bg-green-400"
+                      class="w-9 h-5 bg-red-400 peer-focus:outline-none dark:peer-focus:ring-blue-800 rounded-full peer dark:bg-gray-600 peer-checked:after:translate-x-full peer-checked:after:border-white after:content-[''] after:absolute after:top-[2px] after:left-[2px] after:bg-white after:border-gray-300 after:border after:rounded-full after:h-4 after:w-4 after:transition-all dark:border-gray-500 peer-checked:bg-green-400"
                     ></div>
                     <span
                       class="ml-3 text-sm font-normal text-gray-900 dark:text-gray-300"
@@ -181,7 +181,7 @@ const hideAllTableTd = () => {
                       class="sr-only peer"
                     />
                     <div
-                      class="w-9 h-5 bg-gray-200 peer-focus:outline-none dark:peer-focus:ring-blue-800 rounded-full peer dark:bg-gray-600 peer-checked:after:translate-x-full peer-checked:after:border-white after:content-[''] after:absolute after:top-[2px] after:left-[2px] after:bg-white after:border-gray-300 after:border after:rounded-full after:h-4 after:w-4 after:transition-all dark:border-gray-500 peer-checked:bg-green-400"
+                      class="w-9 h-5 bg-red-400 peer-focus:outline-none dark:peer-focus:ring-blue-800 rounded-full peer dark:bg-gray-600 peer-checked:after:translate-x-full peer-checked:after:border-white after:content-[''] after:absolute after:top-[2px] after:left-[2px] after:bg-white after:border-gray-300 after:border after:rounded-full after:h-4 after:w-4 after:transition-all dark:border-gray-500 peer-checked:bg-green-400"
                     ></div>
                     <span
                       class="ml-3 text-sm font-normal text-gray-900 dark:text-gray-300"
@@ -204,7 +204,7 @@ const hideAllTableTd = () => {
                       class="sr-only peer"
                     />
                     <div
-                      class="w-9 h-5 bg-gray-200 peer-focus:outline-none dark:peer-focus:ring-blue-800 rounded-full peer dark:bg-gray-600 peer-checked:after:translate-x-full peer-checked:after:border-white after:content-[''] after:absolute after:top-[2px] after:left-[2px] after:bg-white after:border-gray-300 after:border after:rounded-full after:h-4 after:w-4 after:transition-all dark:border-gray-500 peer-checked:bg-green-400"
+                      class="w-9 h-5 bg-red-400 peer-focus:outline-none dark:peer-focus:ring-blue-800 rounded-full peer dark:bg-gray-600 peer-checked:after:translate-x-full peer-checked:after:border-white after:content-[''] after:absolute after:top-[2px] after:left-[2px] after:bg-white after:border-gray-300 after:border after:rounded-full after:h-4 after:w-4 after:transition-all dark:border-gray-500 peer-checked:bg-green-400"
                     ></div>
                     <span
                       class="ml-3 text-sm font-normal text-gray-900 dark:text-gray-300"
