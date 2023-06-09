cc67f075b8284774117a4955942fcf7cd1b815a0
wasif_alam <wsfalam84@gmail.com>
2023-06-09 13:02:21 +0530

    edited team team to team name

--- components/Candidates/Table.vue
+++ components/Candidates/Table.vue
@@ -1,86 +1,88 @@
 <script setup>
 import { useCandidate } from '~/composables/candidate.js';
 import { ChevronUpDownIcon } from '@heroicons/vue/24/solid';
-import draggable from 'vuedraggable'
+import draggable from 'vuedraggable';
 import { Drawer } from 'flowbite';
 import useGroup from '~/composables/grouping';
 
-const headers = ref(['Rating', 'Stages', 'Team', 'Applied Date', 'Owner'])
+const headers = ref(['Rating', 'Stages', 'Team', 'Applied Date', 'Owner']);
 
 const headMap = new Map([
   ['Rating', 'isRatingVisible'],
   ['Stages', 'isStagesVisible'],
   ['Team', 'isTeamVisible'],
   ['Applied Date', 'isDateVisible'],
-  ['Owner', 'isOwnerVisible']
-])
+  ['Owner', 'isOwnerVisible'],
+]);
 
 const groupMap = new Map([
   ['Rating', arrangeByRating],
   ['Stages', arrangeByStages],
   ['Team', arrangeByTeam],
-  ['Owner', arrangeByOwner]
-])
+  ['Owner', arrangeByOwner],
+]);
 
-const { DUMMY_DATA } = useTableData()
-const { grouped } = useGroup()
+const { DUMMY_DATA } = useTableData();
+const { grouped } = useGroup();
 const { tableTdVisible } = useHideDropDown();
 
-let byGrouped
+let byGrouped;
 byGrouped = computed(() => {
-
   if (!grouped?.value.active) {
-    return []
+    return [];
   }
-  return groupMap.get(grouped.value.groupedBy)(DUMMY_DATA.value)
-}
-)
+  return groupMap.get(grouped.value.groupedBy)(DUMMY_DATA.value);
+});
 
 function arrangeByRating(data) {
   return data.reduce((acc, user) => {
-    (acc[user.rating] = acc[user.rating] || []).push(user)
-    return acc
-  }
-    , {})
+    (acc[user.rating] = acc[user.rating] || []).push(user);
+    return acc;
+  }, {});
 }
 function arrangeByStages(data) {
   return data.reduce((acc, user) => {
-    (acc[user.stages.state] ||= []).push(user)
-    return acc
-  }, {})
+    (acc[user.stages.state] ||= []).push(user);
+    return acc;
+  }, {});
 }
 function arrangeByTeam(data) {
   return data.reduce((acc, user) => {
-    (acc[user.team.team] ||= []).push(user)
-    return acc
-  }, {})
+    (acc[user.team.name] ||= []).push(user);
+    return acc;
+  }, {});
 }
 function arrangeByOwner(data) {
   return data.reduce((acc, user) => {
-    (acc[user.owner.name] ||= []).push(user)
-    return acc
-  }, {})
+    (acc[user.owner.name] ||= []).push(user);
+    return acc;
+  }, {});
 }
 function changeGroup(list, evt) {
-  console.log('hello')
+  console.log('hello');
   if (evt.added !== undefined) {
     switch (grouped.value.groupedBy) {
       case 'Rating':
-        list[evt.added.newIndex].rating = list[(evt.added.newIndex + 1) % list.length].rating
-        break
+        list[evt.added.newIndex].rating =
+          list[(evt.added.newIndex + 1) % list.length].rating;
+        break;
       case 'Team':
-        list[evt.added.newIndex].team.team = list[(evt.added.newIndex + 1) % list.length].team.team
-        break
+        list[evt.added.newIndex].team.name =
+          list[(evt.added.newIndex + 1) % list.length].team.name;
+        break;
       case 'Stages':
-        list[evt.added.newIndex].stages.state = list[(evt.added.newIndex + 1) % list.length].stages.state
-        break
+        list[evt.added.newIndex].stages.state =
+          list[(evt.added.newIndex + 1) % list.length].stages.state;
+        break;
       case 'Owner':
-        console.log('hello2')
-        list[evt.added.newIndex].owner = { ...list[(evt.added.newIndex + 1) % list.length].owner }
-        break
+        console.log('hello2');
+        list[evt.added.newIndex].owner = {
+          ...list[(evt.added.newIndex + 1) % list.length].owner,
+        };
+        break;
       default:
-        console.log('hello3')
-        break
+        console.log('hello3');
+        break;
     }
   }
 }
@@ -119,15 +121,25 @@ onMounted(() => {
   <CandidatesDetails />
 
   <div class="relative overflow-x-auto rounded-md">
-    <table class="w-full text-sm text-left text-gray-500 border-collapse dark:text-gray-400">
-      <thead v-if="!grouped.active" class="text-sm text-gray-800 bg-white shadow-sm">
+    <table
+      class="w-full text-sm text-left text-gray-500 border-collapse dark:text-gray-400"
+    >
+      <thead
+        v-if="!grouped.active"
+        class="text-sm text-gray-800 bg-white shadow-sm"
+      >
         <draggable v-model="headers" item-key="id" tag="tr">
           <template #header>
             <th scope="col" class="p-4 rounded-l-md">
               <div class="flex items-center">
-                <input id="checkbox-all-search" type="checkbox"
-                  class="w-5 h-5 text-green-400 bg-gray-100 border-gray-300 focus:ring-white focus:ring-0" />
-                <label for="checkbox-all-search" class="sr-only">checkbox</label>
+                <input
+                  id="checkbox-all-search"
+                  type="checkbox"
+                  class="w-5 h-5 text-green-400 bg-gray-100 border-gray-300 focus:ring-white focus:ring-0"
+                />
+                <label for="checkbox-all-search" class="sr-only"
+                  >checkbox</label
+                >
               </div>
             </th>
             <th scope="col" class="px-3 py-3">
@@ -138,7 +150,11 @@ onMounted(() => {
             </th>
           </template>
           <template #item="{ element: header }">
-            <th v-if="tableTdVisible[headMap.get(header)]" scope="col" class="px-3 py-3 cursor-pointer">
+            <th
+              v-if="tableTdVisible[headMap.get(header)]"
+              scope="col"
+              class="px-3 py-3 cursor-pointer"
+            >
               <div class="flex items-center gap-1">
                 <span>{{ header }}</span>
                 <ChevronUpDownIcon class="w-4 h-4" />
@@ -154,13 +170,23 @@ onMounted(() => {
       </thead>
       <thead v-else class="text-sm text-gray-800 bg-white shadow-sm">
         <tr>
-          <draggable v-model="headers" item-key="id" tag="th" class="flex items-center gap-8">
+          <draggable
+            v-model="headers"
+            item-key="id"
+            tag="th"
+            class="flex items-center gap-8"
+          >
             <template #header>
               <th scope="col" class="p-4 rounded-l-md">
                 <div class="flex items-center">
-                  <input id="checkbox-all-search" type="checkbox"
-                    class="w-5 h-5 text-green-400 bg-gray-100 border-gray-300 focus:ring-white focus:ring-0" />
-                  <label for="checkbox-all-search" class="sr-only">checkbox</label>
+                  <input
+                    id="checkbox-all-search"
+                    type="checkbox"
+                    class="w-5 h-5 text-green-400 bg-gray-100 border-gray-300 focus:ring-white focus:ring-0"
+                  />
+                  <label for="checkbox-all-search" class="sr-only"
+                    >checkbox</label
+                  >
                 </div>
               </th>
               <th scope="col" class="px-3 py-3">
@@ -171,7 +197,11 @@ onMounted(() => {
               </th>
             </template>
             <template #item="{ element: header }">
-              <th v-if="tableTdVisible[headMap.get(header)]" scope="col" class="px-3 py-3" >
+              <th
+                v-if="tableTdVisible[headMap.get(header)]"
+                scope="col"
+                class="px-3 py-3"
+              >
                 <div class="flex items-center gap-1">
                   <span>{{ header }}</span>
                   <ChevronUpDownIcon class="w-4 h-4" />
@@ -187,15 +217,26 @@ onMounted(() => {
         </tr>
       </thead>
       <tbody v-if="!grouped.active" class="candidate-tbody">
-        <tr class="text-base border-b bg-gray-50 max-xl:text-sm cursor-pointer" v-for="data in DUMMY_DATA">
-          <CandidatesTableRow :key="data.id" :id="data.id" :candidate="data.candidate" :rating="data.rating"
-            :stages="data.stages" :team="data.team" :appliedDate="data.appliedDate" :owner="data.owner"
-            :headers="headers" />
+        <tr
+          class="text-base border-b bg-gray-50 max-xl:text-sm cursor-pointer"
+          v-for="data in DUMMY_DATA"
+        >
+          <CandidatesTableRow
+            :key="data.id"
+            :id="data.id"
+            :candidate="data.candidate"
+            :rating="data.rating"
+            :stages="data.stages"
+            :team="data.team"
+            :appliedDate="data.appliedDate"
+            :owner="data.owner"
+            :headers="headers"
+          />
         </tr>
       </tbody>
       <tbody v-else>
         <template v-for="(candidates, group) in byGrouped" :key="group">
-          <tr class="px-4 py-3 ">
+          <tr class="px-4 py-3">
             <td colspan="8">
               <div class="flex flex-col gap-2">
                 <span class="">{{ grouped.groupedBy }}</span>
@@ -203,13 +244,26 @@ onMounted(() => {
               </div>
             </td>
           </tr>
-          <draggable :list="candidates" :group="{ name: 'candidates', pull: true, put: true }" itemKey="grouped" tag="tr"
-            @change="changeGroup(candidates, $event)">
+          <draggable
+            :list="candidates"
+            :group="{ name: 'candidates', pull: true, put: true }"
+            itemKey="grouped"
+            tag="tr"
+            @change="changeGroup(candidates, $event)"
+          >
             <template #item="{ element: data }">
               <tr class="cursor-grab">
-                <CandidatesTableRow :key="data.id" :id="data.id" :candidate="data.candidate" :rating="data.rating"
-                  :stages="data.stages" :team="data.team" :appliedDate="data.appliedDate" :owner="data.owner"
-                  :headers="headers" />
+                <CandidatesTableRow
+                  :key="data.id"
+                  :id="data.id"
+                  :candidate="data.candidate"
+                  :rating="data.rating"
+                  :stages="data.stages"
+                  :team="data.team"
+                  :appliedDate="data.appliedDate"
+                  :owner="data.owner"
+                  :headers="headers"
+                />
               </tr>
             </template>
           </draggable>
--- components/Candidates/TableData/Team.vue
+++ components/Candidates/TableData/Team.vue
@@ -1,10 +1,10 @@
 <template>
-    <div class="px-3 py-4">
-        <p class="text-black truncate">{{ content.self }}</p>
-      <p class="text-sm">{{ content.team }}</p>
-    </div>
+  <div class="px-3 py-4">
+    <p class="text-black truncate">{{ content.self }}</p>
+    <p class="text-sm">{{ content.name }}</p>
+  </div>
 </template>
 
 <script setup>
-const {content}=defineProps(['content'])
+const { content } = defineProps(['content']);
 </script>
